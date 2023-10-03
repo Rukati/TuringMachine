6944d5ce7cff3dee453e8964802c652b4adca2ab
Vladimir <132492186+Rukati@users.noreply.github.com>
2023-10-03 17:07:18 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Data.SqlTypes;
 using System.IO;
@@ -41,58 +41,85 @@ class Program
         {
             Console.WriteLine("Error: " + ex.Message);
         }
-        /*        foreach (var tuple in data)
-                {
-                    Console.WriteLine($"Key: {tuple.Item1}");
-                    Console.WriteLine("Data:");
-                    foreach (var itemData in tuple.Item2)
-                    {
-                        Console.WriteLine(itemData);
-                    }
-                    Console.WriteLine();
-                }*/
+/*        foreach (var tuple in data)
+        {
+            Console.WriteLine($"Key: {tuple.Item1}");
+            Console.WriteLine("Data:");
+            foreach (var itemData in tuple.Item2)
+            {
+                Console.WriteLine(itemData[0] + " " + itemData[1] + " " + itemData[2]);
+            }
+        }*/
 
         string input = Console.ReadLine();
 
-        char q = input[0];
+        string q = input[0].ToString();
         int indexQ = 0;
-
-        
-        char nextChar = input[indexQ + 1];
-
-        foreach(var item in data)
+        char nextChar;
+        bool flag = true;
+        while (flag)
         {
-            if (item.Item1 == nextChar.ToString())
+            if (indexQ + 1 < input.Length)
             {
-                for(int i = 0; i < item.Item2[0].Count;i++)
+                nextChar = input[indexQ + 1];  
+            }
+            else
+            {
+                nextChar = 'b';
+                input += 'b';
+            }
+            foreach (var item in data)
+            {
+                if (item.Item1 == nextChar.ToString())
                 {
-                    if (q.ToString() == i.ToString())
+                    for (int i = 0; i < item.Item2.Count; i++)
                     {
+                        if (q == i.ToString())
+                        {
+                            if (item.Item2[i][0] == "-")
+                            {
+                                flag = !flag;
+                                break;
+                            }
+                            q = item.Item2[i][0][1].ToString();
+                            Console.WriteLine("q" + q);
 
-                        char[] charArray = input.ToCharArray();
-
-                        char temp = charArray[indexQ];
-                        charArray[indexQ] = charArray[indexQ + 1];
-                        charArray[indexQ + 1] = temp;
-
-                        input = new string(charArray);
-
-
-                        q = item.Item2[i][0];
-                        if (item.Item2[i][2] == "r")
-                            indexQ+=2;
-                        else
-                            indexQ-=2;
-
-                        Console.WriteLine("q" + q);
-                        break;
+                            char[] charArray = input.ToCharArray();
+                            charArray[indexQ + 1] = item.Item2[i][1][0];
+                            if (item.Item2[i][2] == "r")
+                            {
+                                char temp = q[0];
+                                charArray[indexQ] = charArray[indexQ+1];
+                                charArray[indexQ + 1] = temp;
+                                indexQ += 1;
+                            }
+                            else if (item.Item2[i][2] == "l")
+                            {
+                                char temp = q[0];
+                                charArray[indexQ] = charArray[indexQ - 1];
+                                charArray[indexQ - 1] = temp;
+                                indexQ -= 1;
+                            }
+                            input = new string(charArray);
+                            break;
+                        }
                     }
+                    break;
                 }
             }
+            for (int k = 0; k < input.Length; k++)
+            {
+                if (k == indexQ)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+
+                Console.Write(input[k]);
+                Console.ResetColor();
+            }
+            Console.WriteLine();
+            System.Threading.Thread.Sleep(1000);
         }
-
-        Console.WriteLine(input);
-
 
     }
 }
